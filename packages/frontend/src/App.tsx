3d15f5ee63ace4e6fc798398000391e01158075e
PeiyuanYu <peiyuan.will@gmail.com>
2024-07-23 21:57:12 +1000

    chore: add shadcn and react-router

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,7 +1,27 @@
+import { Card, CardHeader } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="flex items-center justify-center h-screen">
+        <div className="container">
+          <h1 className="text-center text-3xl font-bold mb-4">FXNotify</h1>
+          <Card>
+            <CardHeader className="flex-row justify-between">
+              <Button variant="outline" size="icon">
+                <Plus className="h-6 w-6" />
+              </Button>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </CardHeader>
+          </Card>
+        </div>
+      </div>
     </>
   );
 }
